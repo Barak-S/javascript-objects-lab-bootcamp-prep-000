2982e279ac289366f8c2cea91c7e5e681214a10e
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-16 15:08:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  var newRecipes = object.
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
